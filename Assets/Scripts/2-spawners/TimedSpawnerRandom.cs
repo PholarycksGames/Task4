7d90d25de9fcaa38d33d225ace69db9fef81736e
rafasu4 <raf59101@gmail.com>
2023-04-19 01:47:31 +0300

    🎨format

--- Assets/Scripts/2-spawners/TimedSpawnerRandom.cs
+++ Assets/Scripts/2-spawners/TimedSpawnerRandom.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 /**
  * This component instantiates a given prefab at random time intervals and random bias from its object position.
  */
-public class TimedSpawnerRandom: MonoBehaviour {
+public class TimedSpawnerRandom: MonoBehaviour
+{
     [SerializeField] Mover prefabToSpawn;
     [SerializeField] Vector3 velocityOfSpawnedObject;
     [Tooltip("Minimum time between consecutive spawns, in seconds")] [SerializeField] float minTimeBetweenSpawns = 1f;
     [Tooltip("Maximum time between consecutive spawns, in seconds")] [SerializeField] float maxTimeBetweenSpawns = 3f;
     [Tooltip("Maximum distance in X between spawner and spawned objects, in meters")] [SerializeField] float maxXDistance = 0.5f;
 
-    void Start() {
+    void Start()
+    {
          this.StartCoroutine(SpawnRoutine());    // co-routines
         // _ = SpawnRoutine();                   // async-await
     }
 
-    IEnumerator SpawnRoutine() {    // co-routines
+    IEnumerator SpawnRoutine()
+    {    // co-routines
     // async Task SpawnRoutine() {  // async-await
         while (true) {
             float timeBetweenSpawnsInSeconds = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
--- Assets/Scripts/3-collisions/ShieldThePlayer.cs
+++ Assets/Scripts/3-collisions/ShieldThePlayer.cs
@@ -2,10 +2,12 @@
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class ShieldThePlayer : MonoBehaviour {
+public class ShieldThePlayer : MonoBehaviour
+{
     [Tooltip("The number of seconds that the shield remains active")] [SerializeField] float duration;
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         if (other.tag == "Player") {
             Debug.Log("Shield triggered by player");
             var destroyComponent = other.GetComponent<DestroyOnTrigger2D>();
@@ -16,12 +18,14 @@ public class ShieldThePlayer : MonoBehaviour {
                 // ShieldTemporarily(destroyComponent);                                      // async-await
                 Destroy(gameObject);  // Destroy the shield itself - prevent double-use
             }
-        } else {
+        } else
+        {
             Debug.Log("Shield triggered by "+other.name);
         }
     }
 
-    private IEnumerator ShieldTemporarily(DestroyOnTrigger2D destroyComponent) {   // co-routines
+    private IEnumerator ShieldTemporarily(DestroyOnTrigger2D destroyComponent)
+    {   // co-routines
     // private async void ShieldTemporarily(DestroyOnTrigger2D destroyComponent) {      // async-await
         destroyComponent.enabled = false;
         for (float i = duration; i > 0; i--) {
